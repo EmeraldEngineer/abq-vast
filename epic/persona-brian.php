c9529b9bdb41d48417ca81c1e7b02cd77241e157
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-01-25 13:47:48 -0700

    Created the persona for Brian Murphy (individual user)

--- epic/persona-brian.php
+++ epic/persona-brian.php
@@ -1,1 +1,16 @@
-
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Persona A: Brian Murphy</title>
+	</head>
+	<body>
+		<strong>Name:</strong> Brian Murphy<br>
+		<strong>Age:</strong> 25<br>
+		<strong>Profession:</strong> Teacher<br>
+		<strong>Technology:</strong> Android LG G3, Windows 10<br>
+		<strong>Attitudes and Behaviors:</strong> Brian works an incredible amount of hours. He is knowledgeable about some politics but not necessary involved past voting in elections that matter to them (school board, primary, maybe local). This person uses their desktop to do most of their research but doesn't use many apps on their phone other than sometimes looking up websites.<br>
+		<strong>Frustrations and Needs:</strong> They have limited time since they are a teacher but need to stay on top of local politics, including taxes for their property and how well their school district is going to get funded.<br>
+		<strong>Goals:</strong> Needs a user-friendly website to be able to access the tax information they need to make informed decisions about how to vote in elections (especially how to vote on referendums), how to plan for their school year, and how to talk about spending with their local school board.<br>
+	</body>
+</html>
