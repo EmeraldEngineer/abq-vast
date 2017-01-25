4b6b87053ff02c9437f486ef49ae5f878379dc5f
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-01-25 13:56:19 -0700

    Added the persona for Adriana (Business user)

--- epic/persona-adriana.php
+++ epic/persona-adriana.php
@@ -1,1 +1,18 @@
-
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>Persona B: Adriana Vasquez: Business Owner</title>
+	</head>
+	<body>
+		<main>
+			<strong>Name:</strong> Adriana Vasquez<br>
+			<strong>Age: 30</strong><br>
+			<strong>Profession:</strong> Entrepreneur<br>
+			<strong>Technology:</strong> Apple iOS 7, Apple OS Sierra<br>
+			<strong>Attitudes and Behaviors:</strong> This person would be looking to start their company in Albuquerque. They need to know a cost benefit analysis. Very price sensitive. Looking to diversify their employee-base. They are constantly on their phone and laptop.<br>
+			<strong>Frustrations and Needs:</strong> Adriana needs information in one place. She needs simple information on tax incentives/breaks, cost of living, office space, what other tech companies for the social aspect and the potential for growth.<br>
+			<strong>Goals:</strong> Adriana needs a website/app that will show her the most incentives a business person is looking for in relocating to a new city. This information she will put into her business plan.
+		</main>
+	</body>
+</html>
