54479727ddd701d45693e469b6e45d331531d022
Adam Pedroza <apedroza6@cnm.edu>
2017-01-25 15:57:02 -0700

    Finished system summary in Index made changes to index

--- epic/index.php
+++ epic/index.php
@@ -12,7 +12,7 @@
 		<main>
 			<p>This site is to inform local Albuquerque residents about where and how their tax dollars are being spent. It will also be informative for current
 			and future businesses to see possible tax incentives, government bids and government bonds. Albuquerque Community advocates will be able to gather information about funding projects and events.
-			We hope to inform and display some overall transparency for all of Albuqeurque tax payers.</p>
+			Our overall objective is to display some transparency for all of Albuqeurque tax payers.</p>
 		</main>
 	</body>
 </html>
