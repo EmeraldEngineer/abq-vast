c65a03ee43b14177ed7ab89f7b42f1a7160696a2
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-01-25 13:39:52 -0700

    Created use cases for all the personas.

--- epic/use-cases.php
+++ epic/use-cases.php
@@ -1,1 +1,20 @@
-
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>
+			Use Cases for Show Me The Money
+		</title>
+	</head>
+	<body>
+		<main>
+			<h1>Use Case for Brian Murphy</h1>
+			<p>Brian is busy getting ready for the school year and needs to know what his budget is going to be for his class. He has heard that there are going to be some budget cuts but would like to know where his tax money will be alternatively spent. Brian has heard about showmethemoney.com and took some time to do some keyword searches using the fields. He was then able to identify which projects or salaries might be taking unnecessary resources from school district and felt better prepared to speak with this local school district about his concerns.</p>
+			<h1>Use Case for Adriana Vasquez</h1>
+			<p>Adriana is an entrepreneur from San Francisco who is looking to relocate her tech start-up becuase of the high cost of living in the Bay Area. She wants an area that would also be affordable for her employees. She is looking to Albuquerque because of the incentives the city is giving for tech businesses, the network of other tech industries such as Sanda, Intel and Facebook. She is also interested because of the educational opportunities for local residents to learn software development skills, such as CNM's Deep Dive Coding Bootcamp, which has great reviews in teaching local residences top notch skills.</p>
+			<p>Adriana accesses showmethemoney.com, an App developed by Cohort 15, to help her find more information about tax incentives and other vendor projects that are happening within the city.</p>
+			<h1>Use Case for Maricela Diaz</h1>
+			<p>Maricela is a New Mexico native and runs a nonprofit, Somos Un Pueblo Unido in Albuquerque. Her and her staff are constantly doing research about government spending for her campaigns. Adriana was told about showmethemoney.com from one of her volunteers as a quick way to search for information about city spending. Especially when out of the office and needing to access the infomation on their phones.</p>
+		</main>
+	</body>
+</html>
