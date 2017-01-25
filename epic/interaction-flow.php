51c6a85aa20e65a771fa89d44a9df2635906894b
Valente Meza <kingracoon64@gmail.com>
2017-01-25 14:14:38 -0700

    syntac down for interaction-flow

--- epic/interaction-flow.php
+++ epic/interaction-flow.php
@@ -1,14 +1,21 @@
 <!DOCTYPE html>
 <html lang="eng">
     <head>
-        <meta charset="utf-8"
+        <meta charset="utf-8">
+        <title>interaction flow</title>
     </head>
     <body>
         <header>
-				<h1>TEST</h1>
+				<h1>Interaction flow</h1>
         </header>
         <main>
-
+            <ol>
+                <li>user enters url</li>
+                <li>url is fetched from the database.</li>
+                <li>user enters site and current projects are displayed.</li>
+                <li>user is able to select what project,vendors, and zip code they are interested in.</li>
+                <li></li>
+            </ol>
         </main>
     </body>
 </html>
