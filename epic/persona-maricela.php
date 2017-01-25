e38af7e5c3a137cab686bc71bdcb90cfec5ec611
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-01-25 14:01:25 -0700

    Added the persona for Maricela (Advocacy Organization)

--- epic/persona-maricela.php
+++ epic/persona-maricela.php
@@ -1,1 +1,18 @@
-
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>Persona C: Marcela Díaz: Advocacy Organization</title>
+	</head>
+	<body>
+		<main>
+			<strong>Name:</strong> Marcela Díaz<br>
+			<strong>Age: 38</strong><br>
+			<strong>Profession:</strong> Executive Director of a nonprofit<br>
+			<strong>Technology:</strong> Samsung Galaxy 7 running Marshmallow, Windows 10<br>
+			<strong>Attitudes and Behaviors:</strong> Marcela runs an advocacy nonprofit and is constantly researching different websites/news/resources to help her make decisions on the campaigns of her nonprofit.<br>
+			<strong>Frustrations and Needs:</strong> Because of Marcela's nonprofit budget, time is money. She needs reliable information that will help her understand the data she needs for her campaigns and that it is easily digestible for her constituents.<br>
+			<strong>Goals:</strong> Maricela hopes that showmethemoney.com will be a useful tool for both herself, staff and volunteers to be able to gather data for their campaigns.
+		</main>
+	</body>
+</html>
