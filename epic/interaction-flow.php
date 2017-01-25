cdab5ff6a76ebcfb15f1f9ed9bde25ad04a21d7a
Valente Meza <kingracoon64@gmail.com>
2017-01-25 13:08:26 -0700

    syntac down for interaction-flow

--- epic/interaction-flow.php
+++ epic/interaction-flow.php
@@ -1,1 +1,14 @@
+<!DOCTYPE html>
+<html lang="eng">
+    <head>
+        <meta charset="utf-8"
+    </head>
+    <body>
+        <header>
 
+        </header>
+        <main>
+
+        </main>
+    </body>
+</html>
