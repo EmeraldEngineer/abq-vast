e93274796d016b78808bd63d11b49e16d7778102
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-01-25 17:54:37 -0700

    Updated Adriana's persona to be an investigative journalist instead of an entrepranuer.

--- epic/persona-adriana.php
+++ epic/persona-adriana.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" xmlns="http://www.w3.org/1999/html">
 	<head>
 		<meta charset="UTF-8" />
 		<title>Persona B: Adriana Vasquez: Business Owner</title>
@@ -8,11 +8,11 @@
 		<main>
 			<strong>Name:</strong> Adriana Vasquez<br>
 			<strong>Age: 30</strong><br>
-			<strong>Profession:</strong> Entrepreneur<br>
+			<strong>Profession:</strong> Investigative Journalist<br>
 			<strong>Technology:</strong> Apple iOS 7, Apple OS Sierra<br>
-			<strong>Attitudes and Behaviors:</strong> This person would be looking to start their company in Albuquerque. They need to know a cost benefit analysis. Very price sensitive. Looking to diversify their employee-base. They are constantly on their phone and laptop.<br>
-			<strong>Frustrations and Needs:</strong> Adriana needs information in one place. She needs simple information on tax incentives/breaks, cost of living, office space, what other tech companies for the social aspect and the potential for growth.<br>
-			<strong>Goals:</strong> Adriana needs a website/app that will show her the most incentives a business person is looking for in relocating to a new city. This information she will put into her business plan.
+			<strong>Attitudes and Behaviors:</strong> Adriana is a hard-hitting, independent investigative journalist. She writes for a variety of outlets and also uses her twitter account to reach out to her own audience. She loves using new research apps that make her research fun and effective.<br>
+			<strong>Frustrations and Needs:</strong> Adriana needs apps that she can share on her twitter and other social media channels to help inform her audience about government spending habits. Because she has strong ethics as a journalist, she needs any app she uses to have the most up-to-date and reliable data.<br>
+			<strong>Goals:</strong> Adriana plans to use showmethemoney.com to keep current on city spending as opposed to searching through endless data on the city's website. She also plans to use the graphs created to share in her articles and to her social media channels.
 		</main>
 	</body>
 </html>
