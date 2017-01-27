dc29f07c6760412d5564080e387d80982de50b47
Valente Meza <kingracoon64@gmail.com>
2017-01-27 11:01:40 -0700

    conceptualmodel.php checkbook criteria share

--- epic/conceptualmodel.php
+++ epic/conceptualmodel.php
@@ -2,26 +2,44 @@
 <html lang="eng">
     <head>
         <meta charset="utf-8">
+        <title>
+            Conceptual Model
+        </title>
     </head>
-    <title>Conceptual Model</title>
-    <header><h1>Entities & Attributes</h1></header>
-        <body>
+        <header>
+            <h1>Entities & Attributes</h1>
+        </header>
+    <body>
         <main>
             <h2>Checkbook</h2>
         <ul>
             <li>checkbookId(primary key)</li>
-            <li>checkbookField</li>
-            <li>checkbookOperator</li>
-            <li>checkbookValue</li>
+            <li>checkbookVendor</li>
+            <li>checkbookReferenceNumber</li>
+            <li>checkbookInvoiceNumber</li>
+            <li>checkbookInvoiceDate</li>
+            <li>checkbookPaymentDate</li>
+            <li>checkbookInvoiceAmount</li>
         </ul>
-            <h2>Graph</h2>
-        <ul>
-            <li>graphId</li>
-        </ul>
+            <h2>Criteria</h2>
+            <ul>
+                <li>criteriaId</li>
+                <li>criteriaFieldId</li>
+                <li>criteriaShareId</li>
+                <li>criteriaOperator</li>
+                <li>criteriaValue</li>
+            </ul>
+            <h2>Field</h2>
+            <ul>
+                <li>fieldId</li>
+                <li>fieldName</li>
+                <li>fieldType</li>
+            </ul>
             <h2>Share</h2>
         <ul>
-            <li></li>
+            <li>shareId</li>
+            <li>shareUrl</li>
         </ul>
-        </main>
-        </body>
+            </main>
+    </body>
 </html>
