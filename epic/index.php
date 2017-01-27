0e980884d2a23346658cdf9d33922be23b480b7c
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-01-27 09:54:09 -0700

    Updated personas to include sharing, printing, downloading of raw files and linking to cit councilors.

--- epic/index.php
+++ epic/index.php
@@ -2,13 +2,11 @@
 <html lang="eng">
 	<head>
 		<meta charset="utf-8">
-		<title>System Summery</title>
+			<title>System Summery</title>
 	</head>
 	<body>
-		<header>
-			<h1>System Summery</h1>
-		</header>
 		<main>
+			<h1>System Summery</h1>
 			<p>The overall objective is to create a simple interactive tool that allows anyone to see where and how our local tax money is being spent.
 				The user will be anonymous and will have the option to easily create a graph based on the data they have asked for.
 				Once you give people, advocacy leaders and small business owners this tool, it can be utilized to create accountability and more
--- epic/persona-aaron.php
+++ epic/persona-aaron.php
@@ -8,10 +8,10 @@
 		<strong>Name:</strong> Aaron Murphy<br>
 		<strong>Age:</strong> 25<br>
 		<strong>Profession:</strong> HS Physics Teacher<br>
-		<strong>Technology:</strong> Android LG G3, Windows 10. Brian uses his desktop to do most of his research and school work and his phone for minimal app use with typical on-the-go mobile web browsing<br>
-		<strong>Attitudes and Behaviors:</strong> Brian works an incredible amount of hours at a local APS middle school. He is somewhat knowledgeable about politics, primarily focused on elections that matter to him as an educator like school board, district and city elections.<br>
-		<strong>Frustrations and Needs:</strong> Brian has limited time as an educator but it's important for him to stay on top of local politics. Specifically, what city bonds are getting funded and how his school district will get funded. He has a hard time understanding why the same districts continue to get more funding, while his school which is in desperate need of basic necessities always gets overlooked.<br>
-		<strong>Goals:</strong> Brian wishes there was a website where he could easily access Albuquerque's tax database so he could understand how our education budget is allocated. This would empower him with the facts and information to talk about getting the necessary funds needed, with his local oversized Cheetos(public officials).<br>
+		<strong>Technology:</strong> Android LG G3, Windows 10. Aaron uses his desktop to do most of his research and school work and his phone for minimal app use with typical on-the-go mobile web browsing.<br>
+		<strong>Attitudes and Behaviors:</strong> Aaron works an incredible amount of hours at a local APS middle school. He is somewhat knowledgeable about politics, primarily focused on elections that matter to him as an educator like school board, district and city elections.<br>
+		<strong>Frustrations and Needs:</strong> Aaron has limited time as an educator but it's important for him to stay on top of local politics - specifically, how his taxes are being spent. He has a hard time understanding why the same districts continue to get more funding, while his school which is in desperate need of basic necessities always gets overlooked.<br>
+		<strong>Goals:</strong> Aaron wishes there was a website where he could easily access Albuquerque's spending database so he could understand how his taxes are allocated. He would like to be able to print, email, and share this information, too. This would empower him with the facts and information to talk about getting the necessary funds needed, with his local over-sized Cheetos(public officials).<br>
         <a href="persona-adriana.php">Adriana</a><br>
         <a href="index.php">Index</a><br>
         <a href="persona-maricela.php">Maricela</a><br>
