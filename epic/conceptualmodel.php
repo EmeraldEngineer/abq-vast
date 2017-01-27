4b55a67561a9148e332476a2b94fb293ef0dda96
Valente Meza <kingracoon64@gmail.com>
2017-01-27 11:09:48 -0700

    conceptualmodel.php checkbook criteria share

--- epic/conceptualmodel.php
+++ epic/conceptualmodel.php
@@ -12,34 +12,34 @@
     <body>
         <main>
             <h2>Checkbook</h2>
-        <ul>
-            <li>checkbookId(primary key)</li>
-            <li>checkbookVendor</li>
-            <li>checkbookReferenceNumber</li>
-            <li>checkbookInvoiceNumber</li>
-            <li>checkbookInvoiceDate</li>
-            <li>checkbookPaymentDate</li>
-            <li>checkbookInvoiceAmount</li>
-        </ul>
+                <ul>
+                    <li>checkbookId(primary key)</li>
+                    <li>checkbookVendor</li>
+                    <li>checkbookReferenceNumber</li>
+                    <li>checkbookInvoiceNumber</li>
+                    <li>checkbookInvoiceDate</li>
+                    <li>checkbookPaymentDate</li>
+                    <li>checkbookInvoiceAmount</li>
+                </ul>
             <h2>Criteria</h2>
-            <ul>
-                <li>criteriaId</li>
-                <li>criteriaFieldId</li>
-                <li>criteriaShareId</li>
-                <li>criteriaOperator</li>
-                <li>criteriaValue</li>
-            </ul>
+                <ul>
+                    <li>criteriaId</li>
+                    <li>criteriaFieldId</li>
+                    <li>criteriaShareId</li>
+                    <li>criteriaOperator</li>
+                    <li>criteriaValue</li>
+                </ul>
             <h2>Field</h2>
-            <ul>
-                <li>fieldId</li>
-                <li>fieldName</li>
-                <li>fieldType</li>
-            </ul>
+                <ul>
+                    <li>fieldId</li>
+                    <li>fieldName</li>
+                    <li>fieldType</li>
+                </ul>
             <h2>Share</h2>
-        <ul>
-            <li>shareId</li>
-            <li>shareUrl</li>
-        </ul>
+                <ul>
+                    <li>shareId</li>
+                    <li>shareUrl</li>
+                </ul>
             </main>
     </body>
 </html>
