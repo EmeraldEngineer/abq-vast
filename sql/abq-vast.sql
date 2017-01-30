523481fc48a3945b9ba1468081205c81a92ca116
Valente Meza <kingracoon64@gmail.com>
2017-01-30 16:42:36 -0700

    Update DDL after scrum https://app.asana.com/0/256424297438822/256424297438826

--- sql/abq-vast.sql
+++ sql/abq-vast.sql
@@ -1,37 +1,38 @@
 DROP TABLE IF EXISTS share;
-DROP TABLE IF EXISTS field;
 DROP TABLE IF EXISTS criteria;
+DROP TABLE IF EXISTS field;
 DROP TABLE IF EXISTS checkbook;
 CREATE TABLE checkbook(
   checkbookId INT UNSIGNED AUTO_INCREMENT NOT NULL,
   checkbookVendor VARCHAR(82),
   checkbookReferenceNum VARCHAR(42),
   checkbookInvoiceNum VARCHAR(62),
-  checkbookInvoiceDate DATETIME,
-  checkbookPaymtDate DATETIME,
-  checkbookInvoiceAmount DECIMAL(26, 3),
+  checkbookInvoiceDate DATE,
+  checkbookPaymtDate DATE,
+  checkbookInvoiceAmount DECIMAL(12, 3),
+  INDEX(checkbookVendor),
+  INDEX(checkbookInvoiceDate),
   PRIMARY KEY(checkbookId)
+);
+CREATE TABLE field(
+  fieldId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+  fieldName VARCHAR(64) NOT NULL,
+  fieldType CHAR(1) NOT NULL,
+  PRIMARY KEY(fieldId)
 );
 CREATE TABLE criteria(
   criteriaId INT UNSIGNED AUTO_INCREMENT NOT NULL,
   criteriaFieldId INT UNSIGNED NOT NULL,
   criteriaShareId INT UNSIGNED NOT NULL,
-  criteriaOperator VARCHAR(4),
+  criteriaOperator VARCHAR(4)NOT NULL,
   criteriaValue INT UNSIGNED NOT NULL,
   PRIMARY KEY(criteriaId),
   FOREIGN KEY(criteriaFieldId),
-  UNIQUE(criteriaShareId)
-);
-CREATE TABLE field(
-  fieldId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-  fieldName VARCHAR(64) NOT NULL,
-  fieldType VARCHAR(16) NOT NULL,
-  PRIMARY KEY(fieldId)
+  FOREIGN KEY(criteriaShareId),
 );
 CREATE TABLE share(
   shareId INT UNSIGNED NOT NULL,
-  shareUrl VARCHAR(32),
-  shareImage VARCHAR(32),
-  FOREIGN KEY(shareId),
-  UNIQUE(shareUrl)
-)
+  shareUrl VARCHAR(64) NOT NULL,
+  shareImage VARCHAR(64) NOT NULL,
+  UNIQUE(shareUrl),
+);
