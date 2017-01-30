06a0c880775a23a5301f3aec572af68861b61d51
Adam Pedroza <apedroza6@cnm.edu>
2017-01-30 13:30:53 -0700

    Merge branch 'master' of C:\Users\Adam\Desktop\BootCamp\GIT\showmethemoney with conflicts.

--- epic/use-cases.php
+++ epic/use-cases.php
@@ -1,27 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-        <meta charset="utf-8" />
-        <title>
-            Use Cases for Show Me The Money
-        </title>
-    </head>
-	<body>
-    <main>
-        <h1>Use Case for Aaron Murphy</h1>
-        <p>Brian is out to lunch with some co-workers and the common topic of school funding gets brought up. He lets them know about this new ABQ app that makes it easy to see where all our local tax money is going. He continues to take out his phone and sends each of them a detailed graph of how much funding each Albuquerque school district received in the last 4 years, after only a few clicks</p>
+		<meta charset="utf-8" />
+		<title>
+			Use Cases for Show Me The Money
+		</title>
+	</head>
+	<body>d
+		<main>
+			<h1>Use Case for Aaron Murphy</h1>
+			<p>Brian is out to lunch with some co-workers and the common topic of school funding gets brought up. He lets them know about this new ABQ app that makes it easy to see where all our local tax money is going. He continues to take out his phone and sends each of them a detailed graph of how much funding each Albuquerque school district received in the last 4 years, after only a few clicks</p>
 
-        <h1>Use Case for Adriana Vasquez</h1>
-        <p>Adriana is an investigative journalist looking to find reliable data about how the city pays its vendors: who gets city contracts, how often, and how much they are getting paid by the city. She would also like an app that will translate this data into user-friendly graphs that she can post into her articles.</p>
+			<h1>Use Case for Adriana Vasquez</h1>
+			<p>Adriana is an investigative journalist looking to find reliable data about how the city pays its vendors: who gets city contracts, how often, and how much they are getting paid by the city. She would also like an app that will translate this data into user-friendly graphs that she can post into her articles.</p>
 
-        <h1>Use Case for Maricela Diaz</h1>
-        <p>Maricela and her staff are constantly doing research about government spending for their nonprofit's campaigns. Maricela was told about showmethemoney.com from one of her volunteers as a quick way to search the city's vendor database and create reliable, user-friendly graphs that can then be shared on their social media channels, ultimately assisting their campaigns.</p>
-        <a href="persona-adriana.php">Adriana</a><br>
-        <a href="persona-aaron.php">Brian</a><br>
-        <a href="persona-maricela.php">Maricela</a><br>
-        <a href="interaction-flow.php">Interaction</a><br>
-        <a href="index.php">Index</a><br>
-        <a href="conceptualmodel.php">Conceptual Model</a>
-    </main>
-    </body>
+			<h1>Use Case for Maricela Diaz</h1>
+			<p>Maricela and her staff are constantly doing research about government spending for their nonprofit's campaigns. Maricela was told about showmethemoney.com from one of her volunteers as a quick way to search the city's vendor database and create reliable, user-friendly graphs that can then be shared on their social media channels, ultimately assisting their campaigns.</p>
+            <a href="persona-adriana.php">Adriana</a><br>
+            <a href="persona-aaron.php">Brian</a><br>
+            <a href="persona-maricela.php">Maricela</a><br>
+            <a href="interaction-flow.php">Interaction</a><br>
+            <a href="index.php">Index</a><br>
+		</main>
+	</body>
 </html>
