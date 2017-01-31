9909c701bed2813062bd0cfeb550c139b2a48e2c
Valente Meza <kingracoon64@gmail.com>
2017-01-31 11:56:59 -0700

    accessor method and mutator added to checkbookId https://app.asana.com/0/256424297438822/258718231008065

--- php/classes/Checkbook.php
+++ php/classes/Checkbook.php
@@ -50,6 +50,7 @@ class Checkbook implements \JsonSerializable {
      * @throws \InvalidArgumentException if data types are not valid
      * @throws \RangeException if data values are out of bounds (to long, negarive integers
      * @throw \TypeError if data types violate type
+     * @throw \Exception if some other exception occurs
      **/
     public function __construct(int $newCheckbookId = null, string $newCheckbookVendor, string $newCheckbookReferenceNum, string $newCheckbookInvoiceNum, string $newCheckbookInvoiceDate, string $newCheckbookPaymentDate, string $newCheckbookInvoiceAmount) {
         try{
@@ -63,6 +64,42 @@ class Checkbook implements \JsonSerializable {
         } catch (\InvalidArgumentException $invalidArgument) {
             //** rethrow the exception to the caller */
             throw(new\InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+        } catch(\RangeException $range){
+            //** rethrow the exception to the caller**/
+            throw(new\RangeException($range->getMessage(), 0, $range));
+        } catch (\TypeError $typeError) {
+            //**rethrow the exception to the caller**//
+            throw(new\TypeError($typeError));
+        } catch (\Exception $exception) {
+            throw(new\Exception($exception->getMessage(), 0, $exception));
         }
+    }
+
+    /** accessor method for checkbookId
+     * @return int|null value of checkbook id
+     **/
+    public function getCheckbookId()
+    {
+        return ($this->checkbookId);
+    }
+
+    /**
+     * mutator method for checkbook id
+     * @param int $newCheckbookId new value of checkbook id
+     * @throws \RangeException if $newCheckbookId is not positive
+     * @throws \TypeError if $newCheckbookId is not an integer
+     **/
+    public function setCheckbookId(int $newCheckbookId = null){
+        //**base case: if the checkbook is null
+        if ($newCheckbookId ===null){
+            $this->checkbookId = null;
+            return;
+        }
+        //**verify the checkbook id is positive*/
+        if ($newCheckbookId <= 0){
+            throw(new \RangeException("checkbook is not positive"));
+            //**convert and store the checkbook id
+        }
+        $this->checkbookId = $newCheckbookId;
     }
 }
