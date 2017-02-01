1d04d8d921e6befe97a06ff42474c400807f6e1f
Adam Pedroza <apedroza6@cnm.edu>
2017-02-01 10:45:16 -0700

    began Field class structure

---
+++ php/classes/Field.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * ABQ Vast Field profile
+ *
+ * The field profile collects and compiles all the data the end user has requested.
+ *
+ * @author Adam Pedroza <apedroza6@cnm.edu>
+ * @version
+ **/
+class Field {
+	/**
+	 * id for the Field; This is a primary key
+	 * @var int $fieldId
+	 * **/
+	private $fieldId;
+		/**
+		 * idetifies the Field using a Field Name;
+		 * @var $fieldName
+		 * **/
+	private $fieldName;
+			/**
+			 * idetifies the Field Type;
+			 * @var $fieldType
+			 * **/
+	private $fieldType;
+
+}
+
