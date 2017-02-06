77b5df8ff84caaefba383d63e65e20aad07f9915
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-02-06 14:16:02 -0700

    Added namespace to ShareTest https://app.asana.com/0/252717071352367/260460484968793

---
+++ php/classes/test/ShareTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace Edu\Cnm\AbqVast\Test;
+
+use Edu\Cnm\AbqVast {Share};
+
+// grab the project test parameters
+require_once("AbqVastTest.php");
+
+// grab the class under scrutiny
+require_once(dirname(__DIR__) . "/classes/autoload.php");
+
+/**
+ * Full PHPUnit test for the Share Class
+ *
+ * This is a complete PHPUnit test of the Share Class. It is complete because *ALL* mySQL/PDO enabled methods are tested for both invalid and valid inputs.
+ *
+ * @see Share
+ * @author Sarah Ruth Finkel <sfinkel@cnm.edu>
+ **/
+class ShareTest extends AbqVastTest {
+	/**
+	 * content of the Share
+	 * @var
+	 */
+}
