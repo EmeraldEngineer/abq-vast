9de7dd6eda3aa3ddb485931704ec39d90b094fc1
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-02-06 16:45:13 -0700

    Created syntax corrections to the namespace for ShareTest https://app.asana.com/0/252717071352367/260460484968793

--- php/classes/test/ShareTest.php
+++ php/classes/test/ShareTest.php
@@ -1,13 +1,13 @@
 <?php
 namespace Edu\Cnm\AbqVast\Test;
 
-use Edu\Cnm\AbqVast {Share};
+use Edu\Cnm\AbqVast\{Share};
 
 // grab the project test parameters
 require_once("AbqVastTest.php");
 
 // grab the class under scrutiny
-require_once(dirname(__DIR__) . "/classes/autoload.php");
+require_once(dirname(__DIR__) . "/autoload.php");
 
 /**
  * Full PHPUnit test for the Share Class
@@ -20,6 +20,35 @@ require_once(dirname(__DIR__) . "/classes/autoload.php");
 class ShareTest extends AbqVastTest {
 	/**
 	 * content of the Share
-	 * @var
-	 */
+	 * @var string $VALID_SHARECONTENT
+	 **/
+	protected $VALID_SHARECONTENT = "PHPUnit test passing";
+	/**
+	 * content of the updated Share
+	 * @var string $VALID_SHARECONTENT2
+	 **/
+	protected $VALID_SHARECONTENT2 = "PHPUnit test still passing";
+
+	/**
+	 * create dependent objects before running each test
+	 **/
+	public final function setUp() {
+		// run the default setUp() method first
+		parent::setUp();
+
+		// create and insert a Profile to own the test Share
+		$this->share = new Share(null, "@phpunit", "test@phpunit.de", "+12125551212");
+		$this->share->insert($this->getPDO());
+	}
+
+	/**
+	 * test inserting a valid Share and verify that the actual mySQL data matches
+	 **/
+	public function testInsertValidShare() {
+		//count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("share");
+
+		//create a new share and insert into mySQL
+		$share = new Share(null, $this->share	)
+	}
 }
