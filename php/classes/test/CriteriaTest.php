f12f3080e368768d069e31caa223247778ecd507
Taylor McCarthy <oresshi@gmail.com>
2017-02-05 17:53:08 -0700

    Began writing criteria object test. https://app.asana.com/0/252724212774707/260460484968792

--- php/classes/test/CriteriaTest.php
+++ php/classes/test/CriteriaTest.php
@@ -6,3 +6,57 @@
  * Time: 3:29 PM
  * This is a change.
  */
+namespace Edu\Cnm\AbqVast\Test;
+
+use Edu\Cnm\AbqVast\{criteria};
+
+//grab the project test parameters
+require_once("AbqVastTest.php");
+
+//grab the class under scrutiny
+require_once(dirname(__DIR__) . "/classes/autoload.php");
+
+/**
+ * PHPUnit test for the Criteria class.
+ **/
+
+class CriteriaTest extends AbqVastTest {
+	/**
+	 * content of criteriaId
+	 * @var string $VALID_CRITERIAID
+	 **/
+	protected $VALID_CRITERIAID = "PHPUnit criteriaId test passing";
+
+	/**
+	 * content of criteriaFieldId
+	 * @var $CRITERIAFIELDID criteriafieldid
+	 **/
+	protected $CRITERIAFIELDID = null;
+
+	/**
+	 * content of criteriaShareId
+	 * @var $CRITERIASHAREID criteriashareid
+	 **/
+	protected $CRITERIASHAREID = null;
+
+	/**
+	 * content of criteriaOperator
+	 * @var string $VALID_CRITERIAOPERATOR
+	 **/
+	protected $VALID_CRITERIAOPERATOR = "PHPUnit criteriaOperator test passing";
+
+	/**
+	 * content of criteriaValue
+	 * @var string $VALID_CRITERIAVALUE
+	 **/
+	protected $VALID_CRITERIAVALUE = "PHPUnit criteriaValue test passing";
+
+	/**
+	 * create dependent objects before running each test
+	 **/
+	public final function setUp() {
+		//run the default setUp() method first
+		parent::setUp();
+	}
+
+}
