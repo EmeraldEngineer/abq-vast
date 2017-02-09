175a2e633549e638089b5c986e3fff4d52e9b826
Valente Meza <kingracoon64@gmail.com>
2017-02-09 09:34:55 -0700

    Unit test started now fixing errors https://app.asana.com/0/256424297438822/258718231008065

--- php/classes/test/CheckbookTest.php
+++ php/classes/test/CheckbookTest.php
@@ -78,7 +78,7 @@ class CheckbookTest extends AbqVastTest {
 
         // grab the data from mySQL and enforce the fields match our expectations
         $pdoCheckbook = Checkbook::getCheckbookByCheckbookId($this->getPDO(), $checkbook->getCheckbookId());
-        $this->assertEquals($numRows + 1, $this->getConnection()->getConnection()->getRowCount("checkbook"));
+        $this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("checkbook"));
         $this->assertEquals($pdoCheckbook->getCheckbookId(), $this->checkbook->getCheckbookId());
         $this->assertEquals($pdoCheckbook->getCheckbookInvoiceAmount(), $this->checkbook->getCheckbookInvoiceAmount());
         $this->assertEquals($pdoCheckbook->getCheckbookInvoiceDate(), $this->VALIDCHECKBOOKINVOICEDATE);
