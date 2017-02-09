7176eb9e7bf2d6a4f8a91de7e54d1e2e2e64d334
Valente Meza <kingracoon64@gmail.com>
2017-02-09 08:59:29 -0700

    Unit test started now fixing errors https://app.asana.com/0/256424297438822/258718231008065

--- php/classes/Checkbook.php
+++ php/classes/Checkbook.php
@@ -316,7 +316,7 @@ class Checkbook implements \JsonSerializable {
             }
         } catch(\Exception $exception) {
             // if the row couldn't be converted, rethrow it
-            throw(new \PDOException($exception->getMessage(), null, $exception));
+            throw(new \PDOException($exception->getMessage(), 0, $exception));
         }
         return($checkbook);
     }
