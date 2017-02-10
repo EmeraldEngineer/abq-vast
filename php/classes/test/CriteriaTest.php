82148ab8ede9641d011aaa46bec7d85d4c0e9c28
Taylor McCarthy <oresshi@gmail.com>
2017-02-10 11:23:49 -0700

    deleted testGetAllValidCriteria https://app.asana.com/0/252724212774707/260460484968792

--- php/classes/test/CriteriaTest.php
+++ php/classes/test/CriteriaTest.php
@@ -109,36 +109,4 @@ class CriteriaTest extends AbqVastTest {
 		$this->assertEquals($pdoCriteria->getCriteriaValue(), $this->VALID_CRITERIAVALUE);
 	}
 
-	/**
-	 * test grabbing a criteria that does not exist
-	 **/
-	public function testGetAll() {
-		//grab a criteria id that exceeds the maximum allowable id
-		$criteria = Criteria::getCriteriaByCriteriaId($this->getPDO(), AbqVastTest::INVALID_KEY);
-		$this->assertNull($criteria);
-	}
-
-	public function testGetAllValidCriteria() {
-		// get number for rows and save it for later
-		$numRows = $this->getConnection()->getRowCount("criteria");
-
-		//create a new Criteria and insert into mySQL
-		$criteria = new Criteria(null, $this->field->getFieldId(), $this->share->getShareId(), $this->VALID_CRITERIAOPERATOR, $this->VALID_CRITERIAVALUE);
-		$criteria->insert($this->getPDO());
-
-		//grab the data from mySQL and enforce the fields match out expectations.
-		$results = Criteria::getAllCriteria($this->getPDO());
-		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("criteria"));
-		$this->asserCount(1, $results);
-		$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\AbqVast\\Criteria", $results);
-
-		//grab the result from the array and validate it
-		$pdoCriteria = $results[0];
-		$this->assertEquals($pdoCriteria->getCriteriaId(),$this->criteria->getCriteriaId());
-		$this->assertEquals($pdoCriteria->getFieldId(), $this->field->getFieldId());
-		$this->assertEquals($pdoCriteria->getShareId(), $this->share->getShareId());
-		$this->assertEquals($pdoCriteria->getCriteriaOperator(), $this->VALID_CRITERIAOPERATOR);
-		$this->assertEquals($pdoCriteria->getCriteriaValue(), $this->VALID_CRITERIAVALUE);
-	}
-	
 }
