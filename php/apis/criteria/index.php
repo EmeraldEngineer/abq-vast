ab43918974b76af946fe17c0f30a3c0d8efa5f8b
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-02-20 11:05:20 -0700

    Added the setup https://app.asana.com/0/252717071352367/275588154100889

--- php/apis/criteria/index.php
+++ php/apis/criteria/index.php
@@ -13,31 +13,35 @@ use Edu\Cnm\AbqVast\Criteria;
  * @author Sarah Ruth Finkel <sfinkel@cnm.edu>
  **/
 
-// we determine if we have a GET request. If so, we then process the request.
-if ($method === "GET") {
+//verify the session, start if not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+// prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+// Here we create a new stdClass named $reply. A stdClass is basically an empty bucket that we can use to store things in.
+// We will use this object named $reply to store the results of the call to our API. The status 200 line adds a state variable to $reply called status and initializes it with the integer 200 (success code). The proceeding line adds a state variable to $reply called data. This is where the result of the API call will be stored. We will also update $reply->message as we proceed through the API.
 
 
-// If it is not a GET request, we then proceed here to determine if we have a PUT or POST request.
-} else if($method === "PUT" || $method === "POST") {
-
-	//do setup that is needed for both PUT and POST requests
-
-	//perform the actual put or post
-	if($method === "PUT") {
-		// determines if we have a PUT request. If so we process the request.
-		// process PUT requests here
+try {
+	//grab the mySQL DataBase connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/tweet.ini");
 
 
-	} else if ($method === "POST") {
 
-		// process the POST request  here
+	//determines which HTTP Method needs to be processed and stores the result in $method.
+	$method = array_key_exists("HTTP_x_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
+	//stores the Primary Key for the GET, DELETE, and PUT methods in $id. This key will come in the URL sent by the front end. If no key is present, $id will remain empty. Note that the input is filtered.
+	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+
+
+
+	//Here we check and make sure that we have the Primary Key for the DELETE and PUT requests. If the request is a PUT or DELETE and no key is present in $id, An Exception is thrown.
+	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
+		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
 	}
-
-
-	// if the above requests are neither a PUT or POST delete below
-} else if($method === "DELETE") {
-
-	// process DELETE requests here
-
 }
