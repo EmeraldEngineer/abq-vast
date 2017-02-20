fb2593fa9374de42c0242d401024c8a24fabd145
Adam Pedroza <apedroza6@cnm.edu>
2017-02-20 13:29:37 -0700

    header for share API

---
+++ php/apis/share/index.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once "../../classes/autoloader.php";
+require_once "/lib/xsrf.php"
+require_once("/etc/apache2/abq-vast-mysql/encrypted-config.php");
+
+use Users\Adam\Desktop\BootCamp\GIT\abq-vast\php\apis\Share\index.php;
+
+/**
+ * api for the share class
+ *
+ * @author Adam Pedroza <apedroza6@cnm.edu>
+ **/
+
+//ve
+
+
+
