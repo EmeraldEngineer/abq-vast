bda15dedebdb1d4cacc38778572ac58a57733de6
Taylor McCarthy <oresshi@gmail.com>
2017-02-21 15:44:44 -0700

    added autoload and use statements. created dataset object and defined scale/type of chart. https://app.asana.com/0/256424297438822/275588154100885

--- graphtest.php
+++ graphtest.php
@@ -4,3 +4,22 @@
  *
  **/
 
+/**
+ * include all the classes
+ **/
+require __DIR__.'/vendor/autoload.php';
+
+use CpChart\Chart\Draw;
+use CpChart\Chart\Image;
+use CpChart\Chart\Data;
+
+/* Create your dataset object */
+$myData = new Data();
+/* Add data in your dataset */
+$myData->addPoints(array(VOID,3,4,3,5));
+
+// define chart scale (default) and type of chart (spline)
+$myPicture->drawScale();
+$myPicture->drawSplineChart();
+
+
