557855e8eaaf670f8c60c998d6b3ba66cbcbbf92
Taylor McCarthy <oresshi@gmail.com>
2017-02-21 09:30:36 -0700

    added dirname paths to require once https://app.asana.com/0/inbox/252724212774706/275588154100886/275591278863699

--- public_html/apis/checkbook/index.php
+++ public_html/apis/checkbook/index.php
@@ -1,7 +1,8 @@
 <?php
-require_once "autoloader.php";
-require_once "/lib/xsrf.php";
-require_once "/etc/apache2/capstone-mysql/encrypted-config.php";
+
+require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 use Edu\Cnm\AbqVast\Checkbook;
 
