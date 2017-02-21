7cff187a6e4af7a06bf341d31e893e543b016e14
Taylor McCarthy <oresshi@gmail.com>
2017-02-21 16:12:49 -0700

    graph test working https://app.asana.com/0/256424297438822/275588154100885

--- graphtest.php
+++ graphtest.php
@@ -16,16 +16,16 @@ use CpChart\Chart\Data;
 /* Create your dataset object */
 $myData = new Data();
 /* Add data in your dataset */
-$myData->addPoints(array(VOID,3,4,3,5));
+$myData->addPoints(array(10,3,4,3,5,20));
 
-$myPicture = new Image(700,230,$myData); // width, height, dataset
-$myPicture->setGraphArea(60,40,670,190); // x,y,width,height
+$myPicture = new Image(300,300,$myData); // width, height, dataset
+$myPicture->setGraphArea(30,30,300,250); // x,y,width,height
 //$myPicture->setFontProperties(array("FontName"=>"pChart/fonts/verdana.ttf","FontSize"=>11));
 
 
 // define chart scale (default) and type of chart (spline)
 $myPicture->drawScale();
-$myPicture->drawSplineChart();
+$myPicture->drawAreaChart();
 
 $myPicture->Stroke();
 
