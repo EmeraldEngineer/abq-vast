ebfb03ce4d09344299623ea9ef09538b34587002
Taylor McCarthy <oresshi@gmail.com>
2017-02-21 09:46:20 -0700

    formatting changes to datadownloader.php https://app.asana.com/0/inbox/252724212774706/275588154100886/275591278863699

--- php/classes/DataDownloader.php
+++ php/classes/DataDownloader.php
@@ -8,41 +8,41 @@ require_once("autoload.php");
  * @author Valente Meza vmeza3@cnm.edu
  **/
 class DataDownloader {
-    /**
-     * Vendor checkbook: http://data.cabq.gov/government/vendorcheckbook/VendorCheckBookCABQ-en-us.xml
-     */
-    /**
-     * Gets the metadata from a file url
-     *
-     * @param string %url to grab from
-     * @return mixed stream data
-     * @throws Exception if file doesn't exist
-     **/
+	/**
+	 * Vendor checkbook: http://data.cabq.gov/government/vendorcheckbook/VendorCheckBookCABQ-en-us.xml
+	 */
+	/**
+	 * Gets the metadata from a file url
+	 *
+	 * @param string %url to grab from
+	 * @return mixed stream data
+	 * @throws Exception if file doesn't exist
+	 **/
 
-    public static function getMetaData($url) {
-        $options = [];
-        $options["http"] = [];
-        $options["http"] ["method"] = "HEAD";
-        $context = stream_context_create($options);
+	public static function getMetaData($url) {
+		$options = [];
+		$options["http"] = [];
+		$options["http"] ["method"] = "HEAD";
+		$context = stream_context_create($options);
 
-        // "@" suppresses warnings and errors, fopen opens the actual file
-        $fd = fopen($url, "rb", false, $context);
-        $metaData = stream_get_meta_data($fd);
-        if($fd === false) {
-            throw(new \RuntimeException("unable to open HTTP stream"));
-        }
-        fclose($fd);
-        $header = $metaData["wrapper_data"];
-        foreach($header as $value) {
-            $explodeETag = explode(": ", $value);
-            $findETag = array_search("ETag", $explodeETag);
-            if($findETag !== false) {
-                $eTag = $explodeETag[1];
-            }
-        }
-        var_dump ($eTag);
-        return ($eTag);
-    }
+		// "@" suppresses warnings and errors, fopen opens the actual file
+		$fd = fopen($url, "rb", false, $context);
+		$metaData = stream_get_meta_data($fd);
+		if($fd === false) {
+			throw(new \RuntimeException("unable to open HTTP stream"));
+		}
+		fclose($fd);
+		$header = $metaData["wrapper_data"];
+		foreach($header as $value) {
+			$explodeETag = explode(": ", $value);
+			$findETag = array_search("ETag", $explodeETag);
+			if($findETag !== false) {
+				$eTag = $explodeETag[1];
+			}
+		}
+		var_dump($eTag);
+		return ($eTag);
+	}
 }
 
 $meta = DataDownloader::getMetadata("http://data.cabq.gov/government/vendorcheckbook/VendorCheckBookCABQ-en-us.xml");
