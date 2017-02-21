3f1a86f13e4487a654668ff37416f5c374052a33
Taylor McCarthy <oresshi@gmail.com>
2017-02-21 15:31:27 -0700

    deleted contents, starting again with new tutorial https://app.asana.com/0/256424297438822/275588154100885

--- graphtest.php
+++ graphtest.php
@@ -4,16 +4,3 @@
  *
  **/
 
-// include all the classes
-include("class/Draw.class.php");
-include("class/Image.class.php");
-include("class/Data.class.php");
-
-//create an array to hold the mock variables
-$myArray = array(
-	"name" => "testVendor",
-	"money1" => 1500,
-	"money2" => 2000,
-	"money3" => 2500,
-);
-
