7d4e26495b34a52ac99144f003d8e27baa1821a3
Taylor McCarthy <oresshi@gmail.com>
2017-02-21 17:00:11 -0700

    started testing different graph types and data sources https://app.asana.com/0/256424297438822/275588154100885

--- graphtest.php
+++ graphtest.php
@@ -15,13 +15,22 @@ require __DIR__.'/vendor/autoload.php';
 use CpChart\Chart\Image;
 use CpChart\Chart\Data;
 
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
 /* Create your dataset object */
 $myData = new Data();
 /* Add data in your dataset */
 $myData->addPoints(array(10,3,4,3,5,20));
 
+//create mock dataset matching expected value types from abq data set - name, payment ref number, invoice number, invoice date, payment date, invoice amount
+//$testArray = array("TestVendor",123,42,2013-11-25,2013-12,25,50);
+//$myRealData = new Data();
+//$myRealData->addPoints(array(VOID,3,4,3,5));
+//$testArray->addPoints(array(1,2,5,6));
+
 $myPicture = new Image(300,300,$myData); // width, height, dataset
-$myPicture->setGraphArea(30,30,300,250); // x,y,width,height
+$myPicture->setGraphArea(50,30,300,250); // x,y,width,height
 //$myPicture->setFontProperties(array("FontName"=>"pChart/fonts/verdana.ttf","FontSize"=>11));
 
 
