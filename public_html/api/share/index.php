fef9561d0305d75ceab2dfd18cad9b01b4e44d74
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-02-22 10:38:07 -0700

    Changed $NNN to $id https://app.asana.com/0/256424297438822/275588154100888

--- public_html/api/share/index.php
+++ public_html/api/share/index.php
@@ -32,29 +32,38 @@ try {
 	//stores the Primary Key for the GET, DELETE, and PUT methods in $id. This key will come in the URL sent by the front end. If no key is present, $id will remain empty. Note that the input is filtered.
 	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
 
-	//make sure the NNN is valid for methods that require it
-	if(($method === "DELETE" || $method ==='PUT') && (empty($NNN) === true || $NNN < 0)) {
-		throw(new InvalidArgumentException("NNN cannot be empty or negative", 405));
-	}
-
-	//handle GET request - if NNN is present, that shareId is returned, otherwise all shareId are returned
+	// Here, we determine if the request received is a GET request
 	if($method === "GET") {
 		//set XSRF cookie
-		setXsrfCookie();
+		setXsrfCookie("/");
+		// handle GET request - if id is present, that share is present, that share is returned, otherwise all share are returned
+
+		// Here, we determine if a Key was sent in the URL by checking $id. If so, we pull the requested Share by Share ID from the DataBase and store it in $share.
+		if(empty($id) === false) {
+			$share = Share::getShareByShareId($pdo, $id);
+			if($share !== null) {
+				$reply->data = $share;
+				// Here, we store the retrieved Share in the $reply->data state variable.
+			}
+		}
+		// If there is nothing in $id, and it is a GET request, then we simply return all criteria. We store all the
+	} else if($method === "POST") {
+// this line determines if the request is a POST request
+
 
 		//get a specific shareId or all shareIds and update NNN
-		if(emplty($NNN) === false) {
+		if(empty($id) === false) {
 			$shareId = shareId::getShareByShareId($pdo, $id);
 			if($shareId !== null) {
-				$NNN->data = $shareId;
+				$id->data = $shareId;
 			}
-		} else if(empty($NNN) === false) {
+		} else if(empty($id) === false) {
 			$shareId = ShareId::getShareIdByFieldId($pdo, $fieldId);
 			if($shareId !== null) {
 				$reply->data = $shareId;
 			}
-		} else if(empty($NNN) === false) {
-			$shareId = ShareId::getShareIdByShareUrl($pdo, $NNN);
+		} else if(empty($id) === false) {
+			$shareId = ShareId::getShareIdByShareUrl($pdo, $id);
 			if($shareId !== null) {
 				$reply->data = $shareId;
 			}
