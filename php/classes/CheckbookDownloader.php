de9b1dfcba1ce4f92a3456913acba451b63b10f0
Valente Meza <kingracoon64@gmail.com>
2017-02-22 11:37:29 -0700

    checkbook.xml added https://app.asana.com/0/256424297438822/275588154100883

--- php/classes/CheckbookDownloader.php
+++ php/classes/CheckbookDownloader.php
@@ -10,13 +10,29 @@ require_once("autoload.php");
  * @author Valente Meza vmeza3@cnm.edu
  *
  **/
-    include 'DataDownloader.php';
-    $checkbook = new \SimpleXMLElement($xmlstr);
-    ?>
+/**
+ * @var $previousETag
+ */
+class CheckbookDownloader extends DataDownloader {
+    public static function compareAndDownload() {
+        $checkbookUrl = "http://data.cabq.gov/government/vendorcheckbook/VendorCheckBookCABQ-en-us.xml";
+        $eTag = DataDownloader::getMetaData($checkbookUrl, "checkbook");
+        if($previousETag < $eTag) {
+            $meta = self::BasicSimpleXML($checkbookUrl);
+            $config = writeConfig("/etc/apache2/capstone-mysql/abq-vast.ini");
+            $previousETag = BasicSimpleXML($config["eTag"]);
+            return($previousETag);
+        }
+        return($meta);
+    }
 
-
-
-
+    public static function BasicSimpleXML($xmlstr)
+    {
+        parent::BasicSimpleXML($xmlstr);
+        include 'CheckbookDownloader.php';
+        $checkbook = new \SimpleXMLElement($xmlstr);
+        foreach ($checkbook->vendorname->invoiceamount->invoiceamount as $invoiceamount) {}
+    }
 }
 
 \DataDownloader::getMetaData("http://data.cabq.gov/government/vendorcheckbook/VendorCheckBookCABQ-en-us.xml");
