ec0d3bbae8f970944da5d027c5a6ae7f658efa00
Taylor McCarthy <oresshi@gmail.com>
2017-02-22 14:53:31 -0700

    added y-axis name, attempted to set x-axis name https://app.asana.com/0/256424297438822/275588154100885

--- php/chart.php
+++ php/chart.php
@@ -7,6 +7,7 @@
 require dirname(__DIR__)."/vendor/autoload.php";
 
 //loads the Image and Data files from PChart
+use CpChart\Chart\Draw;
 use CpChart\Chart\Image;
 use CpChart\Chart\Data;
 
@@ -18,12 +19,17 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
 //create dataset object
 $sourceData = new Data();
 //fill dataset object with data - will need to be expanded to pull data mySQL based upon user's input
-$sourceData ->addPoints(array(4200,4400,4800,5600,7200,9001));
+$sourceData->addPoints(array(4200,4400,4800,5600,7200,9001));
+
+$sourceData->setAxisName(0,"HYPE");
+$sourceData->setXAxisName("Time");
 
 //create image canvas (currently set to 16/9 ratio), specify source data
 $chartImage = new Image(800,450,$sourceData);
 //define x,y,width,height of the graph on the canvas
-$chartImage->setGraphArea(50,50,700,350);
+$chartImage->setGraphArea(100,100,700,350);
+
+
 
 //define chart scale - default - some graph types require this
 $chartImage->drawScale();
