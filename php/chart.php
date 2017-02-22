dbc8d159b89196d7092937415bdee95531c1fbda
Taylor McCarthy <oresshi@gmail.com>
2017-02-22 14:00:52 -0700

    added chart image creation, chart type, https://app.asana.com/0/256424297438822/275588154100885

--- php/chart.php
+++ php/chart.php
@@ -4,7 +4,7 @@
  */
 
 //Loads the autoload.php file from the vendor directory.
-require dirname(__DIR__)."vendor/autoload.php";
+require dirname(__DIR__)."/vendor/autoload.php";
 
 //loads the Image and Data files from PChart
 use CpChart\Chart\Image;
@@ -17,8 +17,20 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
 
 //create dataset object
 $sourceData = new Data();
-//fill dataset object with data - will need to be expanded to respond to user's input
-$sourceData ->addPoints(array(42,44,48,56,72));
+//fill dataset object with data - will need to be expanded to pull data mySQL based upon user's input
+$sourceData ->addPoints(array(4200,4400,4800,5600,7200,9001));
 
+//create image canvas (currently set to 16/9 ratio), specify source data
+$chartImage = new Image(800,450,$sourceData);
+//define x,y,width,height of the graph on the canvas
+$chartImage->setGraphArea(50,50,700,350);
+
+//define chart scale - default - some graph types require this
+$chartImage->drawScale();
+//define type of chart, in this case a line chart
+$chartImage->drawLineChart();
+
+//draw image
+$chartImage->stroke();
 
 
