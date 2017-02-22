1000988f22541cc6dcda38508a59476120fe4da7
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-02-22 13:55:18 -0700

    Finished share api before testing https://app.asana.com/0/256424297438822/275588154100888

--- public_html/api/share/index.php
+++ public_html/api/share/index.php
@@ -29,76 +29,80 @@ try {
 	//determine which HTTP method was used
 	$method = array_key_exists("HTTP_x_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
-	//stores the Primary Key for the GET, DELETE, and PUT methods in $id. This key will come in the URL sent by the front end. If no key is present, $id will remain empty. Note that the input is filtered.
+	//sanitize input
 	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+	$shareImage = filter_input(INPUT_POST, "shareImage", FILTER_VALIDATE_STRING);
+	$shareUrl = filter_input(INPUT_POST, "shareUrl", FILTER_SANITIZE_STRING);
 
 	//make sure the is is valid for the methods that require it
 	if(($method === "GET" || $method === "POST") && (empty($id) === true || $id < 0)) {
 		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
 	}
+
+	// handle GET request - if id is present, that share is present, that share is returned, otherwise all share are returned
+	if($method === "GET") {
 		//set XSRF cookie
 		setXsrfCookie("/");
-		// handle GET request - if id is present, that share is present, that share is returned, otherwise all share are returned
 
 		// Here, we determine if a Key was sent in the URL by checking $id. If so, we pull the requested Share by Share ID from the DataBase and store it in $share.
 		if(empty($id) === false) {
 			$share = Share::getShareByShareId($pdo, $id);
 			if($share !== null) {
 				$reply->data = $share;
-				// Here, we store the retrieved Share in the $reply->data state variable.
 			}
 		}
-		// If there is nothing in $id, and it is a GET request, then we simply return all criteria. We store all the
+
+		// If there is nothing in $id, and it is a GET request, then we simply return all share. We store all the share in the $share variable, and then store them in the $reply->data state variable.
+
 	} else if($method === "POST") {
 // this line determines if the request is a POST request
 
+		verifyXsrf();
+		$requestContent = file_get_contents("php://input");
+		// Retrieves the JSON package that the front end sent, and stores it in $requestContent. Here we are using file_get_contents("php://input") to get the request from the front end. file_get_contents() is a PHP function that reads a file into a string. The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read from the front end request which is, in this case, a JSON package.
 
-		//get a specific shareId or all shareIds and update NNN
-		if(empty($id) === false) {
-			$shareId = shareId::getShareByShareId($pdo, $id);
-			if($shareId !== null) {
-				$id->data = $shareId;
-			}
-		} else if(empty($id) === false) {
-			$shareId = ShareId::getShareIdByFieldId($pdo, $fieldId);
-			if($shareId !== null) {
-				$reply->data = $shareId;
-			}
-		} else if(empty($id) === false) {
-			$shareId = ShareId::getShareIdByShareUrl($pdo, $id);
-			if($shareId !== null) {
-				$reply->data = $shareId;
-			}
-		} else {
-			$shareId = ShareId::getAllShareId($pdo);
-			if($shareId !== null) {
-				$reply->data = $shareId;
-			}
+
+		$requestObject = json_decode($requestContent);
+		// This line then decodes the JSON package and stores that result in $requestObject.
+
+
+		//Here we check to make sure that there is content for the Tweet. If $requestObject->criteriaId is empty, an exception is thrown. POST method will use the content to create a new Tweet.
+		if(empty($requestObject->shareId) === true) {
+			throw(new \InvalidArgumentException ("No content for share Id", 405));
 		}
-	} else if($method === "PUT" || $method === "shareImage") {
 
-			verifyXsrf();
-			$requestContent = file_get_contents("php://input");
-			$requestObject = json_decode($requestContent);
+	} else if($method === "POST") {
+		// If it is a POST request we continue to the proceeding lines and make sure that a Profile ID was sent with the request. A new Criteria cannot be created without the crieteria Id. See the constructor in the Criteria class.
+		//make sure criteriaId is available
+		if(empty($requestObject->shareId) === true) {
+			throw(new \InvalidArgumentException ("No Share ID", 405));
+		}
 
-			//make sure shareId is available (required field)
-			if(empty($requestObject->shareImage) === true) {
-				throw(new \InvalidArgumentException ("No content for ShareId.", 405));
-			}
+		// creates a new Criteria object and stores it in $criteria
+		$share = new Share(null, $requestObject->shareImage, $requestObject->shareUrl);
+		// calls the INSERT method in $criteria which inserts the object into the DataBase.
+		$share->insert($pdo);
 
-			//perform the actual put or post
-			if($method === "PUT") {
-				throw(new RuntimeException("shareId does not exist", 404));
-			}
+		// stores the "Criteria created OK" message in the $reply->message state variable.
+		$reply->message = "Share OK";
 
-			// update all attributes
-			$shareId->setShareImage($requestObject->shareImage);
-			$shareId->setShareUrl($requestObject->shareUrl);
-			$shareId->update($pdo);
+	} else {
+		throw (new InvalidArgumentException("Invalid HTTP Method Request"));
+		// If the method request is not GET, PUT, POST, or DELETE, an exception is thrown
+	}
 
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+} catch(TypeError $typeError) {
+	$reply->status = $typeError->getCode();
+	$reply->message = $typeError->getMessage();
+}
 
+header("Content-type: application/json");
+if($reply->data === null) {
+	unset($reply->data);
+}
 
-
-
-
-
+// encode and return reply to front end caller
+echo json_encode($reply);
