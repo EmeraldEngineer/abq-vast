0b426f340673b1a5e7816359cff5a75fa2079ba1
Taylor McCarthy <oresshi@gmail.com>
2017-02-28 08:43:01 -0700

    added abq-vast-app.php, home.php, and .htaccess https://app.asana.com/0/252724212774707/275588154100885

---
+++ public_html/templates/home.php
@@ -0,0 +1,8 @@
+<section class="welcome">
+	<div class="container">
+		<div class="jumbotron text-center">
+			<h1>Welcome to the ABQ-VAST Demo</h1>
+			<p class="lead">Barebones angular2 front end.</p>
+		</div>
+	</div>
+</section>
