2531621b92ea12bcfd2eeb47a41c8af20953395e
Taylor McCarthy <oresshi@gmail.com>
2017-02-28 09:09:10 -0700

    DELETED ALL THE THINGS - KABOOOM! https://app.asana.com/0/252724212774707/275588154100885

--- public_html/templates/home.php
+++
@@ -1,8 +0,0 @@
-<section class="welcome">
-	<div class="container">
-		<div class="jumbotron text-center">
-			<h1>Welcome to the ABQ-VAST Demo</h1>
-			<p class="lead">Barebones angular2 front end.</p>
-		</div>
-	</div>
-</section>
