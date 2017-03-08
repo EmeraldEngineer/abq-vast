9723b221a6f29686a8b1356fffe33258ca46c843
Valente Meza <kingracoon64@gmail.com>
2017-03-08 10:05:33 -0700

    Start Date and End Date added. https://app.asana.com/0/256424297438822/275588154100882

--- epic/template-sandbox/fields.php
+++ epic/template-sandbox/fields.php
@@ -1,43 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8"/>
-		<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge"/>
-		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-COMPATIBLE" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-		<!-- Bootstrap Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
+    <!-- Bootstrap Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
 
-		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous"/>
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous"/>
 
-		<!-- Font Awesome -->
-		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet"/>
+    <!-- Font Awesome -->
+    <link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet"/>
 
-		<!-- Custom Stylesheet -->
-		<link rel="stylesheet" href="styles/styles.css" type="text/css"/>
+    <!-- Custom Stylesheet -->
+    <link rel="stylesheet" href="styles/styles.css" type="text/css"/>
 
-		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
+    <!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
-		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 
-		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-				  crossorigin="anonymous"></script>
+    <!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+            crossorigin="anonymous"></script>
 
-		<script src="lib/jquery-2.1.0.min.js"></script>
-		<script src="src/js/jquery.swipebox.js"></script>
+    <script src="lib/jquery-2.1.0.min.js"></script>
+    <script src="src/js/jquery.swipebox.js"></script>
 
-		<title>Fields</title>
+    <title>Fields</title>
 
-	</head>
+</head>
 <body class="sfooter">
 <div class="sfoooter-content">
     <header>Fields</header>
@@ -46,7 +46,7 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="input-group" id="adv-search">
-                        <input type="text" class="form-control" placeholder="Search for snippets" />
+                        <input type="text" class="form-control" placeholder="Search The Checkbook" />
                         <div class="input-group-btn">
                             <div class="btn-group" role="group">
                                 <div class="dropdown dropdown-lg">
@@ -69,8 +69,12 @@
                                                 <input class="form-control" type="text" />
                                             </div>
                                             <div class="form-group">
-                                                <label for="contain">Date</label>
+                                                <label for="contain">Start Date</label>
                                                 <input class="form-control" type="text" />
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="contain">End Date</label>
+                                                <input class="form-control" type="text"/>
                                             </div>
                                             <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
                                         </form>
@@ -82,9 +86,8 @@
                     </div>
                 </div>
             </div>
-            </div>
         </div>
-    </main>
-
 </div>
+</main>
+
 </body>
