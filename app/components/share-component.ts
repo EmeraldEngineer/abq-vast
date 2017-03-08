5dd3c02ec041cdc43b31293b08e5dbb990d431c8
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-03-08 14:16:14 -0700

    Added sample code

--- app/components/share-component.ts
+++ app/components/share-component.ts
@@ -1,3 +1,34 @@
-/**
- * Created by user on 3/8/17.
- */
+import {Component, OnInit} from "@angular/core";
+import {ActivatedRoute, Params} from "@angular/router";
+import "rxjs/add/operator/switchMap";
+
+@Component({
+	templateUrl: "./templates/rsvp.php"
+})
+
+export class RsvpComponent implements OnInit {
+	alreadyRsvped : boolean = false;
+	invitee : Invitee = null;
+	rsvp : Rsvp = new Rsvp(null, null, "", "", "", 0, new Date());
+	status : Status = null;
+
+	constructor(private inviteeService: InviteeService, private rsvpService: RsvpService, private route: ActivatedRoute) {}
+
+	ngOnInit() : void {
+		this.getInviteeByToken();
+	}
+
+	getInviteeByToken() : void {
+		this.route.params
+			.switchMap((params : Params) => this.inviteeService.getInviteeByInviteeToken(params["inviteeToken"]))
+			.subscribe(reply => {
+				this.invitee = reply.invitee;
+				if(reply.rsvp !== null) {
+					this.rsvp = reply.rsvp;
+					this.alreadyRsvped = true;
+				} else {
+					this.rsvp.rsvpInviteeId = this.invitee.inviteeId;
+				}
+			});
+	}
+}
