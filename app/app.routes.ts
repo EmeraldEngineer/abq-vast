bf22969d1fce729d0f68bd5471ae9aac2ec61df5
Valente Meza <kingracoon64@gmail.com>
2017-03-09 13:19:35 -0700

    fields component added

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -4,14 +4,16 @@ import {HomeComponent} from "./components/home-component";
 import {ShareComponent} from "./components/share-component";
 import {AboutComponent} from "./components/about-component";
 import {MainNavComponent} from "./components/mainnav-component";
+import {FieldsComponent} from "./components/fields-component";
 
-export const allAppComponents = [HomeComponent, AboutComponent, ShareComponent, MainNavComponent];
+export const allAppComponents = [HomeComponent, AboutComponent, ShareComponent, MainNavComponent, FieldsComponent];
 
 export const routes: Routes = [
 	{path: "home", component: HomeComponent},
 	{path: "share/:shareUrl", component: ShareComponent},
 	{path: "about", component: AboutComponent},
-	{path: "", component: HomeComponent}
+	{path: "", component: HomeComponent},
+	{path: "fields", component: FieldsComponent}
 ];
 
 export const appRoutingProviders: any[] = [];
