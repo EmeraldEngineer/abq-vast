669d388f5d6fa92507da86792da11108ca924647
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-03-09 09:12:56 -0700

    Added a mainnav to templates

---
+++ public_html/templates/mainnav.php
@@ -0,0 +1,27 @@
+<header>
+	<nav class="navbar navbar-default">
+		<div class="container">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" routerLink="">ABQ-VAST</a>
+			</div>
+
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+				<ul class="nav navbar-nav navbar-right">
+					<li><a routerLink="/about">About</a></li>
+					<li><a routerLink="/sign-up">VAST Graph</a></li>
+					<li><a href="https://bootcamp-coders.cnm.edu/~sfinkel/abq-vast" target="_blank">ABQ-VAST</a></li>
+				</ul>
+
+			</div><!-- /.navbar-collapse -->
+		</div><!-- /.container-fluid -->
+	</nav>
+</header>
