3817825f4752a76b7cbad4a62dd42f9785ea9ad7
Valente Meza <kingracoon64@gmail.com>
2017-03-12 15:10:15 -0600

    added rest of checkbook list

--- public_html/templates/checkbook.php
+++ public_html/templates/checkbook.php
@@ -2,6 +2,12 @@
 <p>
     <em>Reference Number:</em> {{ checkbook.checkbookReferenceNum }}<br />
     <em>Vendor:</em> {{ checkbook.checkbookVendor }}
+    <em>Invoice Amount</em> {{ checkbook.checkbookInvoiceAmount }}<br/>
+    <em>Invoice Date:</em> {{ checkbook.checkbookInvoiceDate }}<br/>
+    <em>Invoice Number</em> {{ checkbook.checkbookInvoiceNum }}<br/>
+    <em>Payment Date:</em> {{ checkbook.checkbookPaymentDate }}<br/>
+    <em>Reference Number</em> {{ checkbook.checkbookReferenceNum }} <br/>
+    <em>Vendor</em> {{ checkbook.checkbookVendor }} <br/>
 </p>
 <p>
     <a routerLink=""><i class="fa fa-home" aria-hidden="true"></i> Return Home</a>
