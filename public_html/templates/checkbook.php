123be98e0975714283cc64e88bd248fa7f095706
Valente Meza <kingracoon64@gmail.com>
2017-03-11 19:14:24 -0700

    its something!

---
+++ public_html/templates/checkbook.php
@@ -0,0 +1,8 @@
+<h1>Diceware Roll</h1>
+<p>
+    <em>Vendor:</em> {{ checkbook.vendor }}<br />
+    <em>Reference Number:</em> {{ checkbook.reference }}
+</p>
+<p>
+    <a routerLink=""><i class="fa fa-home" aria-hidden="true"></i> Return Home</a>
+</p>
