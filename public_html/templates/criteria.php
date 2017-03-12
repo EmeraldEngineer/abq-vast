b9be134e835685977b685d3ae0c44657fa2977aa
Taylor McCarthy <oresshi@gmail.com>
2017-03-12 16:28:34 -0600

    added criteria template https://app.asana.com/0/252724212774707/275588154100885

---
+++ public_html/templates/criteria.php
@@ -0,0 +1,10 @@
+<h1>Criteria</h1>
+<p>
+	<em>Criteria Operator</em> {{criteria.criteriaOperator}}<br />
+	<em>Criteria Value</em> {{criteria.criteriaValue}}<br />
+
+</p>
+
+<p>
+	<a routerLink=""><i class="fa fa-home" aria-hidden="true"></i> Return Home</a>
+</p>
