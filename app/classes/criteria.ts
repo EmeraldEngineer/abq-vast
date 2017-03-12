da2cd16fd352ab5a85b9c6398d98d66e5c15c401
Taylor McCarthy <oresshi@gmail.com>
2017-03-12 16:03:08 -0600

    created criteria.ts class https://app.asana.com/0/252724212774707/275588154100885

---
+++ app/classes/criteria.ts
@@ -0,0 +1,3 @@
+export class Criteria {
+	constructor(public criteriaId: number, public criteriaFieldId: number, public criteriaShareId: number, public criteriaOperator: string, public criteriaValue: string) {}
+}
