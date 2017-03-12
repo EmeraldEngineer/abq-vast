16e30d536b9aaceb9b65f42a105f22700208f20b
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-03-12 14:07:45 -0600

    Added checkbook component to routes

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -4,6 +4,7 @@ import {HomeComponent} from "./components/home-component";
 import {ShareComponent} from "./components/share-component";
 import {AboutComponent} from "./components/about-component";
 import {MainNavComponent} from "./components/mainnav-component";
+import {CheckbookComponent} from "./components/checkbook-component";
 // import {FieldsComponent} from "./components/fields-component";
 import {GraphComponent} from "./components/graph-component";
 import {NotFoundComponent} from "./components/notfound-component";
@@ -12,7 +13,7 @@ import {BarComponent} from "./components/bar-component";
 // import {PieComponent} from "./components/pie-component";
 
 
-export const allAppComponents = [HomeComponent, AboutComponent, ShareComponent, MainNavComponent, GraphComponent, NotFoundComponent, LineComponent, BarComponent];
+export const allAppComponents = [HomeComponent, AboutComponent, ShareComponent, MainNavComponent, GraphComponent, NotFoundComponent, LineComponent, BarComponent, CheckbookComponent];
 
 export const routes: Routes = [
 	{path: "home", component: HomeComponent},
