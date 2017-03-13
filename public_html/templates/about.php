fa3991ccc25b33d674b15d13860c1fc0b5dea6d5
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-03-13 15:42:45 -0600

    Added more css

--- public_html/templates/about.php
+++ public_html/templates/about.php
@@ -1,18 +1,21 @@
 <main-nav></main-nav>
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-6 -align-center">
-			<h1>ABQ-VAST</h1>
-			<p>VAST Accounts Statistics Tracker</p><br>
-			<p>Utilizing the <a href="https://www.cabq.gov/abq-data/" target="_blank">City of Albuquerque's Open Data Initiative</a>, VAST-ABQ generates a sharable, downloadable graph of the city's Vendor Checkbook data. This data includes all payments made to vendors currently contracted with the city for services – from legal services to office supply purchase.<br><h5>Disclaimer</h5>The city's data is updated on a daily basis with the previous day's transactions. The most up-to-date information will be available daily at 2pm MST. To contact ABQ Data directly, visit <a href="https://www.cabq.gov/abq-data/contact-abq-data">www.cabq.gov</a></p>
+<section class="about">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-6">
+				<h1>ABQ-VAST</h1>
+				<p>VAST Accounts Statistics Tracker</p><br>
+				<p>Utilizing the <a href="https://www.cabq.gov/abq-data/" target="_blank">City of Albuquerque's Open Data Initiative</a>, VAST-ABQ generates a sharable, downloadable graph of the city's Vendor Checkbook data. This data includes all payments made to vendors currently contracted with the city for services – from legal services to office supply purchase.<br><h5>Disclaimer</h5>The city's data is updated on a daily basis with the previous day's transactions. The most up-to-date information will be available daily at 2pm MST. To contact ABQ Data directly, visit <a href="https://www.cabq.gov/abq-data/contact-abq-data">www.cabq.gov</a></p>
 			</div>
 		</div>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-6 center-block"></div>
-			<h1>Entity Relationship Diagram (ERD)</h1>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6"></div>
+				<h1>Entity Relationship Diagram (ERD)</h1>
 				<img src="../images/abqvastERD.svg"/>
+			</div>
 		</div>
 	</div>
-</div>
+</section>
--- public_html/templates/home.php
+++ public_html/templates/home.php
@@ -1,8 +1,9 @@
 <main-nav></main-nav>
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 
-<section class="welcome">
+<section class="home">
 	<div class="container">
-		<div class="jumbotron text-center">
+		<div class="text-center">
 			<h1><i class="fa fa-line-chart" aria-hidden="true"></i> Welcome to ABQ VAST</h1>
 		</div>
 	</div>
