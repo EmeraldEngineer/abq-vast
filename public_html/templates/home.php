403af79b86c0d921335511abb72df4bbea12554f
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-03-13 16:27:51 -0600

    Still working on css and front end...

--- public_html/templates/home.php
+++ public_html/templates/home.php
@@ -1,12 +1,10 @@
 <main-nav></main-nav>
 <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 
-<section>
-	<div class="container-fluid home">
-		<div class="row">
-			<div class="text-center">
-				<h1><i class="fa fa-line-chart" aria-hidden="true"></i> Welcome to ABQ VAST</h1>
-			</div>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-6 home">
+			<h1><i class="fa fa-line-chart" aria-hidden="true"></i> Welcome to ABQ VAST</h1>
 		</div>
 	</div>
-</section>
+</div>
--- public_html/templates/notfound.php
+++ public_html/templates/notfound.php
@@ -1,7 +1,11 @@
+<main-nav></main-nav>
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+
 <div class="container">
 	<div class="row">
-		<div class="col-xs-12 text-center">
-			<h1>No Graph BOO</h1>
+		<div class="col-xs-6 text-center">
+			<h1>404</h1>
+			<p>Sorry but we cannot find the page you are looking for.</p>
 		</div>
 	</div>
 </div>
