0caa422388c79ff933dfce4993842e2f1bc4e8bd
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-03-12 22:12:07 -0600

    Updated field-service

--- app/services/field-service.ts
+++ app/services/field-service.ts
@@ -1,25 +1,25 @@
-// import {Injectable} from "@angular/core";
-// import {Http} from "@angular/http";
-// import {Observable} from "rxjs";
-// import {Field} from "../classes/field";
-// import {BaseService} from "./base-service";
-// @Injectable()
-// export class CheckbookService extends BaseService {
-//     constructor(protected http: Http) {
-//         super(http);
-//     }
-//
-//     private fieldUrl = "api/field/";
-//
-//     getFieldByFieldId() : Observable<Field> {
-//         return(this.http.get(this.fieldUrl)
-//             .map(this.extractData)
-//             .catch(this.handleError));
-//     }
-//
-//     getAllFields() : Observable<Field[]> {
-//         return(this.http.get(this.fieldUrl)
-//             .map(this.extractData)
-//             .catch(this.handleError));
-//     }
-// }
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+import {Observable} from "rxjs";
+import {Field} from "../classes/field";
+import {BaseService} from "./base-service";
+@Injectable()
+export class FieldService extends BaseService {
+    constructor(protected http: Http) {
+        super(http);
+    }
+
+    private fieldUrl = "api/field/";
+
+    getFieldByFieldId(fieldId: number) : Observable<Field> {
+        return(this.http.get(this.fieldUrl + fieldId)
+            .map(this.extractData)
+            .catch(this.handleError));
+    }
+
+    getAllFields() : Observable<Field[]> {
+        return(this.http.get(this.fieldUrl)
+            .map(this.extractData)
+            .catch(this.handleError));
+    }
+}
