c2ee17f6430733835426499ed61061b04a49c46a
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-03-13 09:54:48 -0600

    Deleted fields view

--- public_html/templates/fields.php
+++
@@ -1,9 +0,0 @@
-<h1>Fields</h1>
-<p>
-	<em>Field Name:</em> {{ field.fieldName }}<br />
-	<em>Field Type:</em> {{ field.fieldType }}<br />
-
-</p>
-<p>
-	<a routerLink=""><i class="fa fa-home" aria-hidden="true"></i> Return Home</a>
-</p>
