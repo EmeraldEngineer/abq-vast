6a12e15b6e6f11d6eb850f80363c25d42f8c786f
Valente Meza <kingracoon64@gmail.com>
2017-03-17 11:30:05 -0600

    changed base href

--- webpack/index.php
+++ webpack/index.php
@@ -9,7 +9,7 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
 		<meta charset="UTF-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<base href="<?php echo dirname($_SERVER["PHP_SELF"]) . "/"; ?>" />
+		<base href="/" />
 
 		<title>ABQ-VAST</title>
 	</head>
